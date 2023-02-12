450c2dab471e0916d53a6c513a642a4234e68d52
Aliaksei Pukhalski <90645000+lesik2@users.noreply.github.com>
2023-02-12 14:38:20 +0300

    Update main.java

--- main.java
+++ main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class main {
     public static void main(String[]args)
     {
-        System.out.println("Hello world!Hi to everyone");
+        System.out.println("Hello world!Hi to everyone,I'm so sorry");
         Scanner in=new Scanner(System.in);
         int num=in.nextInt();
         System.out.println(num);
