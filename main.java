fd2c1059066e41e03eed1f9c5c765f6b7d3d0b82
Aliaksei Pukhalski <lesikpuhalskij@gmail.com>
2023-02-12 14:35:09 +0300

    Task 2,Scanner

--- main.java
+++ main.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class main {
     public static void main(String[]args)
     {
         System.out.println("Hello world!");
+        Scanner in=new Scanner(System.in);
+        int num=in.nextInt();
+        System.out.println(num);
     }
 }
